cee4d201ae30817036c103637186020c93f51072
unknown <kananmasajedi@gmail.com>
2022-10-30 21:03:09 +0330

    test version1

---
+++ index.js
@@ -0,0 +1,95 @@
+/*
+    let a;
+    var a;
+    const a;
+    
+    function name(args){ body }
+    var anonymous = function() {}
+    anonymous = () => {}
+    (function(){})()
+
+    var obj = {
+        keya:valuea,
+        keyb:valueb,
+        ...
+    }
+    
+    var arr = [];
+    arr.map();
+    arr.filter();
+    arr.every()
+    arr.some();
+    arr.reduce();
+    arr.fill()
+    arr.shift()
+    arr.unshift()
+    arr.pop()
+    arr.push()
+    arr.slice()
+
+    var str = 'string'
+
+    str.touppercase()
+    str.tolowercase()
+    str.slice();
+    str.substring();
+    str.indexOf();
+    str.lastindexOf();
+    str.charAt();
+    str.trim();
+
+    Math functions
+
+    Math.floor();
+    Math.round();
+    Math.ceil();
+    Math.random();
+    Math.sqrt();
+
+    document.URL
+    document.LastModified
+    document.
+
+    document.getElementById()
+    document.getElementsByTagName()
+    document.getElementsByClassName()
+    document.appendChild();
+    document.createElement();
+    document.FirstChild
+    document.LastChild
+
+    window.history
+    window.history.length
+    window.screen.width
+    window.screen.height
+    window.screenLeft
+    window.screenRight
+    window.screenX
+    window.screenY
+
+    window.open()
+    window.alert()
+    Number()
+    String()
+
+    var date = new Date;
+    date.getDate();
+    date.setDate();
+    date.setMilliseconds();
+    date.getMilliseconds();
+    date.getSeconds();
+    date.setSeconds();
+    date.getMinutes();
+    date.setMinutes();
+    date.getMonth();
+    date.setMonth();
+    date.toString();
+
+    var element = document.getElementById();
+    element.onclick
+    element.addEventListner('event',function,true)
+    <element event='function'></element>
+
+ */
+
+   
